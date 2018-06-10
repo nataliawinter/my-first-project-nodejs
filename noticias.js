0a0322d0c4ab940c948f703c05ae226f6e16a299
nataliawinter3 <nataliawinter@gmail.com>
2018-06-10 12:37:06 -0300

    starting project

---
+++ noticias.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+http.createServer( function(req, res) {
+
+    var category = req.url;
+
+    if (category == '/tecnologia') {
+        res.end('<html><body>Notícias de Tecnologia</body></html>');
+    } else if (category == '/moda') {
+        res.end('<html><body>Notícias de Moda</body></html>');
+    } else if (category == '/beleza') {
+        res.end('<html><body>Notícias de Beleza</body></html>');
+    } else {
+        res.end('<html><body>Portal de Notícias</body></html>');
+    }
+
+}).listen(3000);
