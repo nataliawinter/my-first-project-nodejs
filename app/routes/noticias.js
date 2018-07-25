24bcfa9b8c5beb3e18fbcd1df21e5d8daa785272
Natália Johann Winter <nataliawinter@gmail.com>
2018-07-24 22:32:28 -0300

    [Feature] Mysql connection (#3)
    * changing routes to modules
    
    * inserting mysql package
    
    * creating a connection database

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,4 +1,21 @@
+module.exports = function(app) {
 
-app.get('/noticias', function (req, res) {
-    res.render('noticias/noticias');
-});
+    app.get('/noticias', function (req, res) {
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function(error, result) {
+            console.log(result);
+            res.send(result);
+        });
+        // res.render('noticias/noticias');
+    });
+
+};
